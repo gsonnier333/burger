65f400f13123bfd6b3cd52a1d1d3c66016526d35
Greg Sonnier <gsonnier3@gmail.com>
2021-05-07 16:24:08 -0600

    edited connection.js to use heroku process.env to hold database connection info

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-	host: "localhost",
+	host: process.env.DB_HOST,
 	port: 3306,
-	user: "root",
-	password: "root",
-	database: "burgers_db",
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB,
 });
 
 connection.connect((err) => {
